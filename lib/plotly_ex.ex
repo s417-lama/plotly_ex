608778efaee4d7f832d47263491e19bc06c37c24
slama <s417.lama@gmail.com>
2019-06-05 16:40:50 +0900

    mix new

---
+++ lib/plotly_ex.ex
@@ -0,0 +1,18 @@
+defmodule PlotlyEx do
+  @moduledoc """
+  Documentation for PlotlyEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlotlyEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plotly_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PlotlyExTest do
+  use ExUnit.Case
+  doctest PlotlyEx
+
+  test "greets the world" do
+    assert PlotlyEx.hello() == :world
+  end
+end
